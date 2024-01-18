79e10f13ea982ac2e00c4e5642fd1680afe38f27
Naveen Kumar <nvnkr09@gmail.com>
2024-01-18 18:49:34 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-let arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"]
+let arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
 
 function App() {
   const [firstName, SetFirstName] = useState("");
   const [secondName, SetSecondName] = useState("");
   const [relationship, setRelationship] = useState("");
- 
+  const [btnClicked, setBtnClicked] = useState(false);
+
   console.log(firstName, secondName);
 
-  const handleCalculate = () => {
+  const handleCalculate = (e) => {
+    e.preventDefault();
 
-    if (firstName == "" || secondName == "") {
-      setRelationship("Please Enter valid input")
+    if (firstName.trim() === "" || secondName.trim() === "") {
+      setRelationship("Please Enter valid input");
     }
 
     let str1 = firstName;
@@ -26,40 +28,48 @@ function App() {
     }
     SetFirstName(str1);
     SetSecondName(str2);
-    setRelationship(arr[(firstName.length + secondName.length) % 6])
-  }
+    setRelationship(arr[(firstName.length + secondName.length) % 6]);
+    setBtnClicked(true);
+  };
 
   const handleClear = () => {
     SetFirstName("");
     SetSecondName("");
+    setBtnClicked(false);
     setRelationship("");
   };
 
   return (
     <div id="main">
-      <input
-        type="text"
-        data-testid="input1"
-        placeholder="Enter first name"
-        onChange={(e) => SetFirstName(e.target.value)}
-        value={firstName}
-        name="name1"
-      />
-      <input
-        type="text"
-        data-testid="input2"
-        placeholder="Enter second name"
-        onChange={(e) => SetSecondName(e.target.value)}
-        value={secondName}
-        name="name2"
-      />
-      <button data-testid="calculate_relationship" onClick={handleCalculate}>
-        Calculate Relationship Future
-      </button>
-      <button data-testid="clear" onClick={handleClear}>
-        Clear
-      </button>
-      <h3 data-testid="answer">{relationship}</h3>
+      <form>
+        <input
+          type="text"
+          data-testid="input1"
+          placeholder="Enter first name"
+          onChange={(e) => SetFirstName(e.target.value)}
+          value={firstName}
+          name="name1"
+        />
+        <input
+          type="text"
+          data-testid="input2"
+          placeholder="Enter second name"
+          onChange={(e) => SetSecondName(e.target.value)}
+          value={secondName}
+          name="name2"
+        />
+        <button
+          data-testid="calculate_relationship"
+          type="submit"
+          onClick={handleCalculate}
+        >
+          Calculate Relationship Future
+        </button>
+        <button data-testid="clear" type="reset" onClick={handleClear}>
+          Clear
+        </button>
+      </form>
+      <h3 data-testid="answer">{btnClicked && relationship}</h3>
     </div>
   );
 }
